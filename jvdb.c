5ed0b1dc3ff5d794482781bc3735077f2b6cb1c7
EmilioDam <damcapitol@gmail.com>
2024-01-04 12:14:12 +0100

    Guardar archivos diferentes

--- jvdb.c
+++ jvdb.c
@@ -1,29 +1,39 @@
 #include <stdio.h>
 #include <string.h>
 
+
 int main(int argc, char *argv[]) {
     FILE *archivo;
     char *operacion = argv[1];
+    char *basededatos = argv[2];
+	char *coleccion = argv[3];
+	char *documento = argv[4];
+	char ruta[100];
+	strcpy(ruta,basededatos);
+	strcat(ruta,"/");
+	strcat(ruta,coleccion);
+	strcat(ruta,"/");
+	strcat(ruta,documento);
+	strcat(ruta,".json");
+	
+	printf("La ruta es:  %s",ruta);
+	
     if(strcmp(operacion,"select") == 0){
-    	printf("te doy datos");
+    	archivo = fopen(ruta,"r");
+    	printf("te doy datos:\n");
+    	char linea [1024];
+    	while (fgets(linea, sizeof(linea), archivo) != NULL) {
+	    	printf("Linea: %s\n", linea);
+		}
+    	
 	}else if(strcmp(operacion,"insert") == 0){
-		char *basededatos = argv[2];
-	    char *nombrearchivo = argv[3];
-	    
-	    char ruta[100];
-	    strcpy(ruta,basededatos);
-	    strcat(ruta,"-");
-	    strcat(ruta,nombrearchivo);
-	    strcat(ruta,".txt");
-	    
-	    archivo = fopen(ruta,"a");
+		archivo = fopen(ruta,"w");
 	    char *texto = argv[4];
 	    fputs(strcat(texto,"\n"),archivo);
 	    fclose(archivo);
 	}else{
 		printf("operacioón no válida");
 	}
-    
     
     return 0;
 }
