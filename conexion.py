3771149c46a41d8f2bf25af2b10c1bb434e90583
EmilioDam <damcapitol@gmail.com>
2024-01-04 19:23:20 +0100

    Objeto

--- conexion.py
+++ conexion.py
@@ -1,17 +1,21 @@
 import subprocess
 
-
-class jvdb:
-    def _init_(self,basededatos):
+class Jvdb:	
+    def __init__(self, basededatos):
         self.basededatos = basededatos
-    def insert(self,coleccion,documento,contenido):
+
+    def insert(self, coleccion, documento, contenido):
         self.operacion = "insert"
         self.coleccion = coleccion
         self.documento = documento
         self.contenido = contenido
-            comando = '"c:\\xampp\\htdocs\\jvdb\\jvdb.exe" '+operacion+' '+basededatos+' '+coleccion+' '+documento+' "'+contenido+'"'
-            resultado = subprocess.run(comando,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True)
-            if resultado.returncode == 0:
-                return("ok")
-            else:
-                return("ko")
+        comando = '"c:\\xampp\\htdocs\\jvdb\\jvdb.exe" ' + operacion + ' ' + basededatos + ' ' + coleccion + ' ' + documento + ' "' + contenido + '"'
+        resultado = subprocess.run(comando, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        if resultado.returncode == 0:
+            return "ok"
+        else:
+            return "ko"	
+
+Conexion1 = Jvdb("miempresa")
+
+
